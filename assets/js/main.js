aa6cb52d3c797741877e6e52bbb3cf84b6ea0509
Phil <phil@demeisen.hub>
2014-03-20 18:43:28 +0000

    Adding support for touch events to show labels

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,15 @@ function reqListener () {
 	var sunset = times.sunset;
 	Now.drawSegments(Now.skySegments(sunrise, sunset), 26.5, .5);
 }
+
+window.addEventListener('load', function(){ // on page load
+ 	var labels = document.getElementById('labels');
+	document.body.addEventListener('touchstart', function(e){
+  		document.body.className = 'touched';
+	}, false);
+
+	document.body.addEventListener('touchend', function(e){
+  		document.body.className = '';
+	}, false);
+ 
+}, false)
